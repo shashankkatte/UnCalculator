c740cadef52e51ea894599225d26d3b3cf839110
shashankkatte <shashankanand.katte@gmail.com>
2020-04-21 23:36:22 -0400

    Some more demo with strings and template literal, escape characters.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,11 @@
 let currentResult = 0;
+const defaultResult =0;
 
 currentResult = currentResult +10;
 
-let calculationDescription = '(0+10)';
+// let calculationDescription = '('+defaultResult+'+10)';
+
+// Or we could use a template literal, you can add line breaks here (redundant whitespaces). but in browser you'll need to style using white-space:pre
+let calculationDescription = `( ${defaultResult} + 10 )`;
 
 outputResult(currentResult, calculationDescription);
